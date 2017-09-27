a8e56226093487de695998fc00f6513d049fffb4
PatrickHickey <patrickstephenhickey@gmail.com>
2017-09-26 21:06:42 -0700

    project1

--- catalog/app.js
+++ catalog/app.js
@@ -9,6 +9,12 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Home
 app.get('/', (req, res, next) => {
   res.send('Please use /api/products');
@@ -38,17 +44,42 @@ app.get('/api/products/:id', (req, res, next) => {
 
 // Add Product
 app.post('/api/products', (req, res, next) => {
-  res.send('Add Products');
+  db.products.insert(req.body, (err, doc) => {
+    if(err){
+      res.send(err);
+    }
+    console.log('Adding Product...');
+    res.json(doc);
+  });
 });
 
 // Update Product
 app.put('/api/products/:id', (req, res, next) => {
-  res.send('Update Product '+req.params.id);
+  db.products.findAndModify({query: {_id: mongojs.ObjectId(req.params.id)},
+    update:{
+      $set:{
+        name: req.body.name,
+        category: req.body.category,
+        details: req.body.details
+      }},
+      new: true }, (err, doc) => {
+        if(err){
+          res.send(err);
+        }
+        console.log('Updating Product...');
+        res.json(doc);
+      })
 });
 
 // Delete Product
 app.delete('/api/products/:id', (req, res, next) => {
-  res.send('Delete Product '+req.params.id);
+  db.products.remove({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+    if(err){
+      res.send(err);
+    }
+    console.log('Removing Product...');
+    res.json(doc);
+  });
 });
 
 app.listen(port, () => {
