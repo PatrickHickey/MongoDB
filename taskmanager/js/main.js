83f0a48531e2fc6cc438158c0451b5adafbf9f3e
PatrickHickey <patrickstephenhickey@gmail.com>
2017-10-02 23:47:04 -0700

    project2

--- taskmanager/js/main.js
+++ taskmanager/js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
   getTasks();
+  getCategoryOptions();
 });
 
 const apiKey = 'oxMq5u0BVZQIzp6m-mXHkQcrWUd20Jw2';
 
 function getTasks(){
   $.get('https://api.mlab.com/api/1/databases/taskmanager_test/collections/tasks?apiKey='+apiKey, function(data){
-    let output = '<ul class="list-group">';
+    var output = '<ul class="list-group">';
     $.each(data, function(key, task){
       output += '<li class="list-group-item">';
       output += task.task_name+'<span class="due_on">[Due on '+task.due_date+']</span>'
@@ -17,5 +18,16 @@ function getTasks(){
     });
     output += '</ul>';
     $('#tasks').html(output);
+  });
+}
+
+function getCategoryOptions(){
+  $.get('https://api.mlab.com/api/1/databases/taskmanager_test/collections/categories?apiKey='+apiKey, function(data){
+    var output;
+    $.each(data, function(key, category){
+      output += '<option value="'+category.category_name+'">'+category.category_name+'</option>';
+    });
+    output += '</ul>';
+    $('#category').append(output);
   });
 }
