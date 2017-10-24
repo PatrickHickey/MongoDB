72ac04fbbf62aa4988ffae4d21e7e8428ef6ef54
PatrickHickey <patrickstephenhickey@gmail.com>
2017-10-23 23:55:10 -0700

    project3

--- invoicr/app.js
+++ invoicr/app.js
@@ -12,6 +12,13 @@ const app = express();
 // Port
 const port = 3000;
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Client folder
 app.use(express.static(__dirname+'/client'));
 
