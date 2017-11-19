64c5846d3b463d048ed336c78b579f79f78efffd
PatrickHickey <patrickstephenhickey@gmail.com>
2017-11-18 20:14:05 -0800

    project4

--- mongochat/server.js
+++ mongochat/server.js
@@ -7,6 +7,41 @@ mongo.connect('mongodb://127.0.0.1/mongochat', (err, db) => {
   }
   console.log('Server Started & MongoDB Connected...');
   client.on('connection', (socket) => {
+    let cha = db.collection('chats');
 
+    sendStatus = (s) => {
+      socket.emit('status', s);
+    }
+
+    chat.find().limit(100).sort({_id:1}).toArray((err, res) => {
+      if(err){
+        throw err;
+      }
+      socket.emit('output', res);
+    });
+
+    socket.on('input', (data) => {
+      const name = data.name;
+      const message = data.message;
+
+      if(name == '' || message == ''){
+        sendStatus('Please fill in the name and message');
+      } else {
+        chat.insert({name: name, message: message}, () => {
+          client.emit('output', [data]);
+
+          sendStatus({
+            message: 'Message Sent',
+            clear: true
+          });
+        });
+      }
+    });
+
+    socket.on('clear', () => {
+      chat.remove({}, () => {
+        socket.emit('cleared');
+      });
+    });
   });
 });
