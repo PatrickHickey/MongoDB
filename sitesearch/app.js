3d4838a66a130c3691c8619f1d41eb8b65bb46fb
PatrickHickey <patrickstephenhickey@gmail.com>
2017-11-28 23:06:07 -0800

    project5

--- sitesearch/app.js
+++ sitesearch/app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/sitesearch');
+var db = mongoose.connection;
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var websites = require('./routes/websites');
 
 var app = express();
 
@@ -23,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/websites', websites);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
