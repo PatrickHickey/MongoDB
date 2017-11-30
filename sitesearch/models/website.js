e127e24e6cb002f15b3c70148e1d2abc03f6838a
PatrickHickey <patrickstephenhickey@gmail.com>
2017-11-29 19:07:24 -0800

    project5

--- sitesearch/models/website.js
+++ sitesearch/models/website.js
@@ -20,7 +20,19 @@ var websiteSchema = mongoose.Schema({
   }
 });
 
+websiteSchema.plugin(searchPlugin, {
+  fields: ['title', 'description', 'url']
+});
+
 var Website = module.exports = mongoose.model('Website', websiteSchema);
+
+module.exports.searchWebsites = function(searchText, callback){
+  Website.search(searchText, {title:1, description:1, url:1}, {
+    conditions:{title:{$exists: true}, description:{$exists: true}, url:{$exists: true}},
+    sort: {title:1},
+    limit:50
+  }, callback);
+}
 
 module.exports.addWebsite = function(website, callback){
   Website.create(website, callback);
