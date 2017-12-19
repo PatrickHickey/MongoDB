b6d6de796acb040da6fb73c36db10f05ec2cf00d
PatrickHickey <patrickstephenhickey@gmail.com>
2017-12-19 02:42:35 -0800

    project6

--- geeklogger/api/controllers/LogsController.js
+++ geeklogger/api/controllers/LogsController.js
@@ -6,12 +6,26 @@
  */
 
 module.exports = {
-	list: function(req, res){
-		Logs.find({}).exec(function(err, logs){
-			if(err){
-				res.send(500, {error: 'Database Error'});
-			}
-			res.view('list', {logs: logs});
-		});
-	}
+ 	list: function(req, res){
+ 		Logs.find({}).exec(function(err, logs){
+ 			if(err){
+ 				res.send(500, {error: 'Database Error'});
+ 			}
+ 			res.view('list', {logs: logs});
+ 		});
+ 	},
+ 	add: function(req, res){
+ 		return res.view('add');
+ 	},
+ 	create: function(req, res){
+ 		var name = req.body.name;
+ 		var text = req.body.text;
+
+ 		Logs.create({name:name, text: text}).exec(function(err){
+ 			if(err){
+ 				res.send(500, {error: 'Database Error'});
+ 			}
+ 			res.redirect('/logs/list');
+ 		});
+ 	}
 };
